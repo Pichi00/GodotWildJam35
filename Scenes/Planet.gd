0e5d192a068aba253ebdc915db930c1ca9a14526
Piotr Kołodziejski <piter.kolodziejski@gmail.com>
2021-07-14 16:27:46 +0200

    Add journal entries

--- Scenes/Planet.gd
+++ Scenes/Planet.gd
@@ -3,15 +3,19 @@ extends Area2D
 var revealed = false
 var rng = RandomNumberGenerator.new()
 var type = Global.PLANETS.DIRT
+var unlocked = false
 
 func _ready():
 	set_type()
 	$TextureProgress.value = 100
+	unlocked = Global.planets_unlocked[type]
 
 func _on_Planet_body_entered(body):
 	if !revealed:
 		$AnimationPlayer.play("Reveal")
 		revealed = true
+	if !unlocked:
+		Global.planets_unlocked[type] = true
 
 func set_type():
 	rng.randomize()
--- global.gd
+++ global.gd
@@ -5,3 +5,4 @@ var player_max_hp = 10
 var player_hp = 10
 
 enum PLANETS {DIRT,GREEN, PINK, ICE, EARTH, X}
+var planets_unlocked = [false,false,false,false,false,false]
