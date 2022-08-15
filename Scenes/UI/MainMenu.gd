0c9f7f5da048657e39775de16e8c011bf5a5c7f3
Piotr Kołodziejski <piotr.kolodziejski00@hotmail.com>
2022-08-15 14:33:51 +0200

    Add make_array function

--- Scenes/UI/MainMenu.gd
+++ Scenes/UI/MainMenu.gd
@@ -10,14 +10,19 @@ func _on_PlayButton_pressed():
 	Global.player_damage = 1
 	Global.player_speed = 90
 	Global.player_speed_lvl = 1
-	Global.planets_unlocked = [false,false,false,false,false,false,false,false,false]
-	Global.planets_visited = [0,0,0,0,0,0,0,0,0]
+	Global.planets_unlocked = make_array(9, false)
+	Global.planets_visited = make_array(9, 0)
 	Global.money = 0
 	Global.player_max_hp = 10
 	Global.player_hp = 10
 	get_tree().change_scene("res://Scenes/Levels/Level.tscn")
 
-
 func _on_ContinueButton_pressed():
 	Global.load_game()
 	get_tree().change_scene("res://Scenes/Levels/Level.tscn")
+
+func make_array(arr_size:int, arr_value) -> Array:
+	var arr = Array()
+	arr.resize(arr_size)
+	arr.fill(arr_value)
+	return arr
